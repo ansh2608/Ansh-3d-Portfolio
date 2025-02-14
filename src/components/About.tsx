3f74d0e54ad532cd0fc553eb5b38206b870eb943
Ansh Porwal <98257000+ansh2608@users.noreply.github.com>
2025-02-15 00:35:04 +0530

    About.tsx

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -6,9 +6,11 @@ const About = () => {
       <div className="about-me">
         <h3 className="title">About Me</h3>
         <p className="para">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic quis
-          dolores numquam iusto Ratione earum ducimus autem id iure pariatur
-          dolorum quae maiores.
+          I am Ansh Porwal, a software engineer with experience in machine learning, data science, 
+        and full-stack development. As a research intern at IIT BHU and founder of 
+        VIT Bhopal's Blockchain Club, I have worked on projects in AI, NLP, and cloud technologies. 
+        I enjoy solving complex problems, with over 1,000 data structures and algorithms solutions 
+        and strong competition rankings, including Google HashCode (Global Rank: 3450/25,000+).
         </p>
       </div>
     </div>
